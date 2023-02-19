ce41e4b8e5c53067721e1785adfca0838caaae83
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-02-19 14:28:46 +0700

    feat: update articles image

--- src/components/article-updates/index.tsx
+++ src/components/article-updates/index.tsx
@@ -6,21 +6,21 @@ export const ArticleVsUpdates = () => (
         <h3 className={styles.headerTitle}>Articles & Updates</h3>
         <div className={styles.articleList}>
             <div className={styles.article}>
-                <img className={styles.image} src="/consulting.png" />
+                <img className={styles.image} src="/article1.png" />
                 <p className={styles.date}>13-Dec,2022</p>
                 <h4 className={styles.title}>
                     Ultricies mi vestibulum varius habitant arcu blandit tempus.
                 </h4>
             </div>
             <div className={styles.article}>
-                <img className={styles.image} src="/consulting.png" />
+                <img className={styles.image} src="/article2.png" />
                 <p className={styles.date}>13-Dec,2022</p>
                 <h4 className={styles.title}>
                     Lorem ipsum dolor sit amet consectetur.
                 </h4>
             </div>
             <div className={styles.article}>
-                <img className={styles.image} src="/consulting.png" />
+                <img className={styles.image} src="/article3.png" />
                 <p className={styles.date}>13-Dec,2022</p>
                 <h4 className={styles.title}>
                     Viverra ut metus sit mattis ultrices felis aliquam.
